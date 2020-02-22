9540c758fe7fb9ba06913ce2170e2f59f2cff774
fgvfgfg564 <fgvfgfg564@126.com>
2020-02-22 10:12:03 +0800

    add background image

--- src/RVRmain.java
+++ src/RVRmain.java
@@ -1,7 +1,12 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import Characters.*;
+import javax.swing.JPanel;
 
 public class RVRmain {
 
@@ -11,14 +16,36 @@ public class RVRmain {
 }
 
 class MainFrame extends JFrame {
-    public MainFrame()
-    {
-        setSize(1280,720);
+
+    public MainFrame() {
+        setSize(1280, 591);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setIconImage(ImageIO.read(new File("")));
+        ImagePainter battlefield = new ImagePainter("src/scenery/battlefield.png",0,0);
         setVisible(true);
 
         Jessica jessica = new Jessica();
+        battlefield.paintComponent(getGraphics());
         jessica.paintComponent(getGraphics());
+    }
+}
+
+class ImagePainter extends JPanel {
+
+    BufferedImage img;
+    int x, y;
+
+    public ImagePainter(String pathname, int x, int y) {
+        try {
+            img = ImageIO.read(new File(pathname));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(img, x, y, this);
     }
 }
